f1089f5d61ba0cbd55f2c199e4188ac433184617
Wee Sritippho <weeaix@gmail.com>
2020-12-07 03:40:01 +0700

    display land on map

--- client/src/landshow/index.js
+++ client/src/landshow/index.js
@@ -7,7 +7,15 @@ import {
   TableRow
 } from "@material-ui/core";
 import React, { Component } from "react";
-import { withRouter } from "react-router"
+import { withRouter } from "react-router";
+import { GeoJSON as GeoJSONObj } from "leaflet";
+import {
+  GeoJSON,
+  MapConsumer,
+  MapContainer,
+  TileLayer
+} from "react-leaflet";
+import * as Wkt from "wicket";
 
 class LandShow extends Component {
 
@@ -16,7 +24,8 @@ class LandShow extends Component {
     this.state = {
       infos: [
         { label: '...', value: '' }
-      ]
+      ],
+      geom: null
     };
   }
 
@@ -30,12 +39,18 @@ class LandShow extends Component {
     const infos = [
       { label: 'วันที่ประกาศ', value: issueDateString }
     ]
-    this.setState({ infos });
+    const geom = new Wkt.Wkt(land.geom);
+    this.setState({ infos, geom: geom.toJson() });
     console.log(land);
   };
 
   render() {
     const { match } = this.props;
+    const landGeom = () => {
+      if (this.state.geom) {
+        return <GeoJSON key={match.params.id} data={this.state.geom} />
+      }
+    }
     return (
       <div>
         <h1>รูปแปลงรหัส {match.params.id}</h1>
@@ -51,6 +66,22 @@ class LandShow extends Component {
             </TableBody>
           </Table>
         </TableContainer>
+        <MapContainer id="single-land-map" center={[16, 100]} zoom={6} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {landGeom()}
+          <MapConsumer>
+            {(map) => {
+              if (this.state.geom) {
+                const bounds = (new GeoJSONObj(this.state.geom)).getBounds();
+                map.fitBounds(bounds);
+              }
+              return null;
+            }}
+          </MapConsumer>
+        </MapContainer>
       </div>
     );
   }
